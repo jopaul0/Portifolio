bc2dc37e319ad1130d82330007c4bb9d0c682b26
João Paulo <jopaulo.as8@gmail.com>
2025-05-09 08:55:06 -0300

    refactor: Mudando a estrutura da section de projetos.

---
+++ script/projects.js
@@ -0,0 +1,22 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const listItems = document.querySelectorAll('.list-project');
+    const projectSections = document.querySelectorAll('.project');
+
+    listItems.forEach(item => {
+        item.addEventListener('click', () => {
+            const selectedProject = item.getAttribute('data-project');
+
+            // Remove classe 'active' de todos os projetos e miniaturas
+            listItems.forEach(i => i.classList.remove('active'));
+            projectSections.forEach(section => section.classList.remove('active'));
+
+            // Adiciona classe 'active' ao projeto e à miniatura clicada
+            item.classList.add('active');
+            const projectToShow = document.querySelector(`.project[data-project="${selectedProject}"]`);
+            if (projectToShow) {
+                projectToShow.classList.add('active');
+            }
+        });
+    });
+});
